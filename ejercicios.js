d9fd52b96bb17b5e250c36093f04b70795b13c6e
Roxsy Velasquez <roxsy.lj@gmail.com>
2017-07-14 16:09:59 -0500

    ejercicio 2-gladys

---
+++ ejercicios.js
@@ -0,0 +1,6 @@
+//ejercicio 2 de GLADYS
+function potencia(numero,exponente){
+resultado=Math.pow(numero,exponente); //La función  Math.pow() devuelve la  base elevada al exponente , esto es, baseexponente
+return resultado;
+}
+console.log(potencia(5,2));
